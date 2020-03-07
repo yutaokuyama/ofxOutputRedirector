499ce1b5b04a02e14d91734a37fce7c230cd9cad
okuyama <yutaokuyam@gmail.com>
2020-03-07 23:53:38 +0900

    Add show toggle

--- src/ofxOutputRedirector.cpp
+++ src/ofxOutputRedirector.cpp
@@ -41,28 +41,30 @@ void ofxOutputRedirector::update(){
     outs[1].clear();
     readFromFile();
     for(int i= 0;i<2;i++){
-    alpha[i] += (0.0-alpha[i])/10.0;
-}
+        alpha[i] += (0.0-alpha[i])/10.0;
+    }
 }
 
 void ofxOutputRedirector::draw(){
     ofEnableAlphaBlending();
-    ofSetColor(ofFloatColor(0.3,0.3,0.3,0.3));
-       ofDrawRectangle(ofGetWidth()/4,0.0,ofGetWidth()/2*1.5,ofGetHeight());
-    ofSetColor(ofFloatColor(0.0,1.0,0.0,alpha[0])); ofDrawRectangle(ofGetWidth()/4,0.0,ofGetWidth()/2*1.5,ofGetHeight()/2);
-    ofSetColor(ofFloatColor(1.0,0.0,0.0,alpha[1])); ofDrawRectangle(ofGetWidth()/4,ofGetHeight()/2,ofGetWidth()/2*1.5,ofGetHeight()/2);
-    
-    ofSetColor(ofFloatColor(0.8,0.8,0.8,1.0));
-    ofDrawLine(ofGetWidth()/4, 0.0,ofGetWidth()/4,ofGetHeight());
-    ofDrawLine(ofGetWidth()/4, ofGetHeight()/2,ofGetWidth(),ofGetHeight()/2);
-    
-   
-    ofSetColor(ofFloatColor(1.0));
-    for(int i= 0;i<outs[0].size();i++){
-        ofDrawBitmapString(outs[0][i],ofGetWidth()/4,11*(1+i));
-    }
-    for(int i= 0;i<outs[1].size();i++){
-        ofDrawBitmapString(outs[1][i],ofGetWidth()/4,11*(i+1)+ofGetHeight()/2);
+    if(show){
+        ofSetColor(ofFloatColor(0.3,0.3,0.3,0.3));
+        ofDrawRectangle(ofGetWidth()/4,0.0,ofGetWidth()/2*1.5,ofGetHeight());
+        ofSetColor(ofFloatColor(0.0,1.0,0.0,alpha[0])); ofDrawRectangle(ofGetWidth()/4,0.0,ofGetWidth()/2*1.5,ofGetHeight()/2);
+        ofSetColor(ofFloatColor(1.0,0.0,0.0,alpha[1])); ofDrawRectangle(ofGetWidth()/4,ofGetHeight()/2,ofGetWidth()/2*1.5,ofGetHeight()/2);
+        
+        ofSetColor(ofFloatColor(0.8,0.8,0.8,1.0));
+        ofDrawLine(ofGetWidth()/4, 0.0,ofGetWidth()/4,ofGetHeight());
+        ofDrawLine(ofGetWidth()/4, ofGetHeight()/2,ofGetWidth(),ofGetHeight()/2);
+        
+        
+        ofSetColor(ofFloatColor(1.0));
+        for(int i= 0;i<outs[0].size();i++){
+            ofDrawBitmapString(outs[0][i],ofGetWidth()/4,11*(1+i));
+        }
+        for(int i= 0;i<outs[1].size();i++){
+            ofDrawBitmapString(outs[1][i],ofGetWidth()/4,11*(i+1)+ofGetHeight()/2);
+        }
     }
     
     ofDisableAlphaBlending();
